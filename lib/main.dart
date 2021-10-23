cd577a251fb003828ad3858b63f0e102e8b1529e
Israel Blancas <iblancasa@gmail.com>
2021-10-23 20:21:44 +0200

    Ok, Flutter
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,148 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/gestures.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_fadein/flutter_fadein.dart';
+import 'package:flip_card/flip_card.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-          body: CustomScrollView(
-    slivers: <Widget>[
-      SliverAppBar(
-        pinned: true,
-        expandedHeight: 300.0,
-        backgroundColor: Colors.cyan,
-        flexibleSpace: FlexibleSpaceBar(
-          title: const Text("Israel Blancas @iblancasa"),
-          background: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("assets/header.jpg"), fit: BoxFit.cover)),
-          ),
-        ),
-      ),
-      SliverList(delegate: SliverChildListDelegate(getGridChildren()))
-    ],
-  ))));
+  runApp(MyApp());
 }
 
-LayoutBuilder getGrid() {
-  return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-    return GridView.count(
-      primary: false,
-      padding: const EdgeInsets.all(20),
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 10,
-      crossAxisCount: getAxistCount(context),
-      children: getGridChildren(),
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Israel Blancas - iblancasa",
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
-  });
-}
-
-int getAxistCount(BuildContext context) {
-  if (MediaQuery.of(context).size.width >= 1024) {
-    return 2;
   }
-  return 1;
 }
 
-List<Widget> getGridChildren() {
-  return [
-    Card(
-        shape: BeveledRectangleBorder(),
-        margin: const EdgeInsets.all(15),
-        elevation: 10,
-        child: Padding(
-            padding: EdgeInsets.all(20),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text(
-                "Hi! Welcome to my website",
-                style: TextStyle(fontSize: 50),
-              ),
-              Padding(
-                  padding: EdgeInsets.fromLTRB(0, 5, 15, 0),
-                  child: Text(
-                    "I love all the things related to technology (specially, software development). For this reason, I spend my free time working on open source projects, attending to conferences or researching about new technologies. Linux, Python, Golang, Docker and GitHub user.",
-                    overflow: TextOverflow.clip,
-                    style: TextStyle(fontSize: 25),
-                    textAlign: TextAlign.justify,
-                  )),
-            ]))),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-    textCard("Title", "Subtitle", Icons.ac_unit),
-  ];
-}
-
-AppBar appBar() {
-  return AppBar(title: const Text("My application"), centerTitle: true);
-}
-
-Card textCard(String title, String subtitle, IconData icon) {
-  return Card(
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-    margin: const EdgeInsets.all(15),
-    elevation: 10,
-    child: Column(
-      children: <Widget>[
-        ListTile(
-          contentPadding: const EdgeInsets.fromLTRB(15, 10, 25, 10),
-          title: Text(title),
-          subtitle: Text(subtitle),
-          leading: Icon(icon),
-          onTap: () {}, // Keep the effect
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Israel Blancas - iblancasa'),
         ),
-      ],
-    ),
-  );
+        body: ListView(
+          children: <Widget>[
+            getCard(context),
+          ],
+        ));
+  }
+
+  Widget getCard(BuildContext context) {
+    return FlipCard(
+      fill: Fill
+          .fillBack, // Fill the back side of the card to make in the same size as the front.
+      direction: FlipDirection.HORIZONTAL, // default
+      front: miCardImage(context),
+      back: miCardImage(context),
+    );
+  }
+
+  Card miCardImage(BuildContext context) {
+    Flex myWidget;
+
+    var width = MediaQuery.of(context).size.width;
+    if (width > 450) {
+      myWidget = Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: getButtons(),
+      );
+    } else {
+      myWidget = Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: getButtons(),
+      );
+    }
+
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      margin: const EdgeInsets.all(15),
+      elevation: 10,
+      child: Column(
+        children: <Widget>[
+          FadeIn(
+              curve: Curves.easeIn,
+              duration: const Duration(milliseconds: 4000),
+              child: const Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Image(
+                    image: AssetImage("assets/header.jpg"),
+                  ))),
+          FadeIn(
+              curve: Curves.easeIn,
+              duration: const Duration(milliseconds: 11000),
+              child: const Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
+                child: Text("Hi, my name is Israel!",
+                    style: TextStyle(fontSize: 30)),
+              )),
+          FadeIn(
+              curve: Curves.easeOutQuint,
+              duration: const Duration(milliseconds: 4000),
+              child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        const TextSpan(
+                          text: "Quality engineer @ ",
+                          style: TextStyle(color: Colors.black, fontSize: 20),
+                        ),
+                        TextSpan(
+                          text: 'Red Hat',
+                          style:
+                              const TextStyle(color: Colors.blue, fontSize: 20),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              launch('https://www.redhat.com/');
+                            },
+                        ),
+                      ],
+                    ),
+                  ))),
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 25),
+            child: myWidget,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+List<Widget> getButtons() {
+  void _launchURL(String url) async =>
+      await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
+
+  return [
+    TextButton.icon(
+        onPressed: () async => {_launchURL("https://github.com/iblancasa")},
+        icon: const FaIcon(FontAwesomeIcons.github),
+        label: const Text("GitHub")),
+    TextButton.icon(
+        onPressed: () async => {_launchURL("https://twitter.com/iblancasa")},
+        icon: const FaIcon(FontAwesomeIcons.twitter),
+        label: const Text("Twitter")),
+    TextButton.icon(
+        onPressed: () async =>
+            {_launchURL("https://es.stackoverflow.com/users/4887/iblancasa")},
+        icon: const FaIcon(FontAwesomeIcons.stackOverflow),
+        label: const Text("Stack Overflow")),
+    TextButton.icon(
+        onPressed: () async =>
+            {_launchURL("https://es.linkedin.com/in/iblancasa")},
+        icon: const FaIcon(FontAwesomeIcons.linkedin),
+        label: const Text("LinkedIn")),
+    TextButton.icon(
+        onPressed: () async => {_launchURL("https://dev.to/iblancasa")},
+        icon: const FaIcon(FontAwesomeIcons.dev),
+        label: const Text("Dev.to")),
+  ];
 }
